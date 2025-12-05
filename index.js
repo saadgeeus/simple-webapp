c84050775ba89b9ba81ebda5339ba408f765ace7
saadgeeus <saadgeeus@gmail.com>
2025-12-05 19:24:01 +0500

    Add HTML route and static folder

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-const http = require('http');
+const express = require('express');
+const path = require('path');
+const app = express();
+
 const port = process.env.PORT || 3000;
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type':'text/plain'});
-  res.end('Hello from Azure DevOps pipeline!\n');
+
+// Serve static files from "public" folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route: /Home -> serve index.html
+app.get('/Home', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-server.listen(port, () => console.log(`Server running on port ${port}`));
+
+// Default route
+app.get('/', (req, res) => {
+  res.send("Welcome! Try /Home");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
